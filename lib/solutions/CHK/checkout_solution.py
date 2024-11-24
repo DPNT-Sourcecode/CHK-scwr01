44b9a4998a1e192d28c21a15160937c8712e0700
sbx_user1051 <sbx_user1051@169.254.31.22>
2024-11-24 22:21:16 +0000

    Sun Nov 24 22:21:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,17 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+def get_prices()->dict:
+    return {
+        "A":50,
+        "B":30,
+        "C":20,
+        "D":15
+    }
+    
+def get_offers()->dict:
+    
+
 def checkout(skus):
     """checkout supermarkt checkout and calculate the total prices
 
@@ -11,5 +22,6 @@ def checkout(skus):
     Returns:
         Integer: total prices
     """
+    
     return 123
 
--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -2,4 +2,5 @@ from lib.solutions.CHK import checkout_solution
 import pytest
 
 class TestCheckout():
-    def test_checkout_1(self)
+    def test_checkout_1(self):
+        assert checkout_solution("A")==50
