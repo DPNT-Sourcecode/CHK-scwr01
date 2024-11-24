2018412691cad1e10f0d47147b776674762d5ea1
sbx_user1051 <sbx_user1051@169.254.31.22>
2024-11-24 22:15:15 +0000

    Sun Nov 24 22:15:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """checkout supermarkt checkout and calculate the total prices
+
+    Args:
+        skus (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return 123
+
