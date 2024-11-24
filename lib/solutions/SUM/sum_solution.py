3b9fe35cf21c582802fc385a0d45467c281c968c
sbx_user1051 <sbx_user1051@169.254.31.22>
2024-11-24 21:49:49 +0000

    Sun Nov 24 21:49:49 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,13 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x:int, y:int)->int:
+    """compute summary challenge
+
+    Args:
+        x (_type_): positive integer 0-100
+        y (_type_): positive integer 0-100
+
+    Returns:
+        _type_: int
+    """
+    return x+y
+
