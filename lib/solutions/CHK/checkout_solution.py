146e7184deab4f32a76c287d6607212c3f1e9d74
sbx_user1051 <sbx_user1051@169.254.31.22>
2024-11-25 01:49:50 +0000

    Mon Nov 25 01:49:50 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,9 +12,9 @@ def get_prices()->dict:
     return {
         "A": 50, "B": 30, "C": 20, "D": 15, "E": 40,
         "F": 10, "G": 20, "H": 10, "I": 35, "J": 60,
-        "K": 80, "L": 90, "M": 15, "N": 40, "O": 10,
-        "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20,
-        "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50
+        "K": 70, "L": 90, "M": 15, "N": 40, "O": 10,
+        "P": 50, "Q": 30, "R": 50, "S": 20, "T": 20,
+        "U": 40, "V": 50, "W": 20, "X": 17, "Y": 20, "Z": 21
     }
 
 def get_offers()->dict:
@@ -114,3 +114,4 @@ def checkout(skus):
         else:
             total_price+=count*prices[item]
     return total_price
+
