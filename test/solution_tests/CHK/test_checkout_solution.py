4131ba6b1deec9fc9fbeaf2bf3fcf4211e43e324
sbx_user1051 <sbx_user1051@169.254.31.22>
2024-11-25 00:30:16 +0000

    Mon Nov 25 00:30:16 UTC 2024

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -4,7 +4,7 @@ import pytest
 def test_checkout_1(self):
     assert checkout_solution("AAA")==130
     assert checkout_solution("A") ==50
-    
+    assert checkout_solution("AA") == 100
     
 if __name__ == "__main__":
     pytest.main()
