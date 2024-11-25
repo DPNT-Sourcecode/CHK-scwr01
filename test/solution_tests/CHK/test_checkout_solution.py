54f7170017c11945485f20dfa8121fdfd441ca09
sbx_user1051 <sbx_user1051@169.254.31.22>
2024-11-25 00:27:16 +0000

    Mon Nov 25 00:27:16 UTC 2024

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -1,7 +1,10 @@
 from lib.solutions.CHK import checkout_solution
 import pytest
 
-class TestCheckout():
-    def test_checkout_1(self):
-        assert checkout_solution("AAA")==130
-        assert checkout_solution("A") ==50
+def test_checkout_1(self):
+    assert checkout_solution("AAA")==130
+    assert checkout_solution("A") ==50
+    
+    
+if __name__ == "__main__":
+    pytest.main()
