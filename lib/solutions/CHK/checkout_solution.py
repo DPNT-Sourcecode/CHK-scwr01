8d97098bb3a0131bf44e12c04ca268d07db72fa8
sbx_user1051 <sbx_user1051@169.254.31.22>
2024-11-25 01:42:45 +0000

    Mon Nov 25 01:42:45 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,12 +10,11 @@ def get_prices()->dict:
         dict: prices
     """
     return {
-        "A":50,
-        "B":30,
-        "C":20,
-        "D":15,
-        "E":40,
-        "F":10
+        "A": 50, "B": 30, "C": 20, "D": 15, "E": 40,
+        "F": 10, "G": 20, "H": 10, "I": 35, "J": 60,
+        "K": 80, "L": 90, "M": 15, "N": 40, "O": 10,
+        "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20,
+        "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50
     }
 
 def get_offers()->dict:
@@ -28,7 +27,11 @@ def get_offers()->dict:
         "A":[(3,130),(5,200)],
         "B":[(2,45)], 
         "E":(2,"B"),
-        "F":(3,"F")
+        "F":(3,"F"),
+        "H":[(10,80),(5,45)],
+        "K":[(2,150)],
+        "N":(3,"M"),
+        "P":[(5,200)]
     }
 
 def validate_skus(skus,prices) ->bool:
@@ -107,4 +110,3 @@ def checkout(skus):
         else:
             total_price+=count*prices[item]
     return total_price
-
